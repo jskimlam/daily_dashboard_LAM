e78448bb19d8e1a1481db4d92596b8cf054cd063
jskimlam <zard770@naver.com>
2026-02-06 19:45:56 +0900

    Update image.py

--- image.py
+++ image.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import os
 
-# 원본은 images/latest.png에 있고, 결과물도 images/latest.webp로 저장해야 합니다.
 input_path = 'images/latest.png'
 output_path = 'images/latest.webp'
 
@@ -11,9 +10,9 @@ def process_image():
         return
 
     with Image.open(input_path) as img:
-        img = img.convert("RGB")
+        img = img.convert("RGB") # PNG의 투명도를 처리하기 위해 RGB로 변환
         img.save(output_path, 'WEBP', quality=80)
-        print("Success!")
+        print("Success: latest.webp created")
 
 if __name__ == "__main__":
     process_image()
